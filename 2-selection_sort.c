92edd02e6c335ad7999f1a68af222575a1be55c7
christian <ch.ov@outlook.com>
2022-06-21 11:02:38 +0000

    betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,29 +6,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t x, y;
-    int min, aux, tmp, bool = 0;
+	size_t x, y;
+	int min, aux, tmp, bool = 0;
 
-    for (x = 0; x < size; x++)
-    {
-        min = array[x];
-        aux = x;
-        for (y = x; y < size; y++)
-        {
-            if (array[y] < min)
-            {
-                min = array[y];
-                aux = y;
-                bool = 1;
-            }
-        }
-        if (bool)
-        {
-            tmp = array[x];
-            array[x] = min;
-            array[aux] = tmp;
-            print_array(array, size);
-            bool = 0;
-        }
-    }
+	for (x = 0; x < size; x++)
+	{
+		min = array[x];
+		aux = x;
+		for (y = x; y < size; y++)
+		{
+			if (array[y] < min)
+			{
+				min = array[y];
+				aux = y;
+				bool = 1;
+			}
+		}
+		if (bool)
+		{
+			tmp = array[x];
+			array[x] = min;
+			array[aux] = tmp;
+			print_array(array, size);
+			bool = 0;
+		}
+	}
 }
